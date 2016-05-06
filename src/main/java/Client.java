22cceeb11f97f7af12e54968f500fb9a75592c99
Peter Beach <peterbeach2@gmail.com>
2016-05-06 13:20:38 -0700

    salon and salon_test databases are set up.

---
+++ src/main/java/Client.java
@@ -0,0 +1,16 @@
+public class Client {
+  private int id;
+  private String name;
+
+  public Client(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+}
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,32 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ClientTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM clients *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Client_instantiatesCorrectly_true() {
+    Client myClient = new Client("James");
+    assertEquals(true, myClient instanceof Client);
+  }
+
+  @Test
+  public void getName_clientInstantiatesWithName_String() {
+    Client myClient = new Client("Tiffany");
+    assertEquals("Tiffany", myClient.getName());
+  }
+
+}
