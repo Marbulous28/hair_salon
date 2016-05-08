6a88696101fffb4613fd437c585c158876ef8464
Peter Beach <peterbeach2@gmail.com>
2016-05-08 10:52:44 -0700

    Implemented delete method for clients

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -84,4 +84,14 @@ public class ClientTest {
     assertEquals("Dax", Client.find(myClient.getId()).getName());
   }
 
+  @Test
+  public void delete_deletesClient_true() {
+    Client myClient = new Client("James", 1);
+    myClient.save();
+    int myClientId = myClient.getId();
+    myClient.delete();
+    assertEquals(null, Client.find(myClientId));
+  }
+
+
 }
